09e0bd16c4fb62a0d687792aa41b755cf4172dd0
kenan kurda <Intecbrussel2122@gmail.com>
2023-11-19 12:38:24 +0100

    bugs fixed voor header

--- BankAppYieldReturn/Program.cs
+++ BankAppYieldReturn/Program.cs
@@ -12,8 +12,6 @@ namespace BankAppYieldReturn
         {
             Console.OutputEncoding = Encoding.UTF8;
 
-            int vasteLengte = 20;
-
             Rekening rekening = new Rekening("123-456789-01");
 
             rekening.VoerTransactieUit(5000.00m, "Storting");
@@ -22,29 +20,7 @@ namespace BankAppYieldReturn
             rekening.VoerTransactieUit(500.00m, "Storting");
             rekening.VoerTransactieUit(-600.00m, "Geld opname");
 
-            int i = 0;
-
-            var saldoArray = rekening.GenereerSaldo();
-            foreach (decimal saldo in saldoArray)
-            {
-                if (i < rekening.Transacties.Length && rekening.Transacties[i] != null)
-                {
-                    Transactie transactie = rekening.Transacties[i];
-
-                    string datum = transactie.Datum.ToShortDateString().PadRight(vasteLengte); 
-                    string bedrag = transactie.Bedrag.ToString("C").PadLeft(vasteLengte);
-                    string omschrijving = transactie.Omschrijving.PadRight(vasteLengte);
-                    string saldomschrijving = "Saldo na transactie:".PadRight(vasteLengte);
-                    string saldoTekst = saldo.ToString("C").PadLeft(vasteLengte);
-                    Console.WriteLine($"{datum} {bedrag} {omschrijving} {saldomschrijving} {saldoTekst}");
-                }
-                else
-                {
-                    break;
-                }
-
-                i++;
-            }
+            rekening.ToonTransacties();
 
         }
     }
--- BankAppYieldReturn/Rekening.cs
+++ BankAppYieldReturn/Rekening.cs
@@ -11,11 +11,6 @@ namespace BankAppYieldReturn
         public string Rekeningnummer { get; set; }
         public decimal Saldo { get; set; }
         private Transactie[] _transacties = new Transactie[100];
-        public Transactie[] Transacties
-        {
-            get { return _transacties; }
-        }
-
 
         private int i = 0; // aantal transacties
         public Rekening(string rekeningnummer)
@@ -28,21 +23,50 @@ namespace BankAppYieldReturn
             i++;
 
             Saldo += bedrag;
+            
         }
 
+        public void ToonTransacties()
+        {
+            int i = 0;
+            int vasteLengte = 20;
+            Console.WriteLine("Zonder Yield");
 
-        //public void ToonTransacties()
-        //{
-        //    foreach (Transactie item in _transacties)
-        //    {
-        //        if (item == null)
-        //            break;
-        //        Console.WriteLine($"{item.Datum} {item.Bedrag} {item.Omschrijving} Saldo:{Saldo}");
-        //    }
-        //}
+            decimal[] saldoArray = BerekenSaldo();
 
+            string datumHeader = "Datum van transactie".PadRight(vasteLengte);
+            string bedragHeader = "Bedrag".PadLeft(vasteLengte);
+            string spatie = "     ".PadLeft(5);
+            string omschrijvingHeader = "Omschrijving".PadRight(vasteLengte);
+            string saldoTekstHeader = "Saldo".PadLeft(vasteLengte);
 
-        public decimal[] GenereerSaldo()
+            Console.WriteLine($"{datumHeader} {bedragHeader} {spatie} {omschrijvingHeader} {saldoTekstHeader}");
+
+            foreach (decimal saldo in saldoArray)
+            {
+                if (i < _transacties.Length && _transacties[i] != null)
+                {
+                    Transactie transactie = _transacties[i];
+                   
+                    string datum = transactie.Datum.ToShortDateString().PadRight(vasteLengte);
+                    string bedrag = transactie.Bedrag.ToString("C").PadLeft(vasteLengte);
+                   
+
+                    string omschrijving = transactie.Omschrijving.PadRight(vasteLengte);
+                    string saldoTekst = saldo.ToString("C").PadLeft(vasteLengte);
+                    Console.WriteLine($"{datum} {bedrag} {spatie} {omschrijving} {saldoTekst}");
+                }
+                else
+                {
+                    break;
+                }
+
+                i++;
+            }
+            
+        }
+
+        public decimal[] BerekenSaldo()
         {
             decimal[] saldoNaTransactie = new decimal[i];
             decimal Saldo = 0;
@@ -51,15 +75,16 @@ namespace BankAppYieldReturn
             foreach (var transactie in _transacties)
             {
                 if (transactie == null || index >= i)
+                {
                     break;
-
+                }
+                   
                 Saldo += transactie.Bedrag;
                 saldoNaTransactie[index++] = Saldo;
             }
-
+           
             return saldoNaTransactie;
+            
         }
-
-
     }
 }
